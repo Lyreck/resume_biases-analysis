2cf4bb01b2d75d6e7f3bcf7ba09b1acf39e0aa22
Leo <63649449+Lyreck@users.noreply.github.com>
2025-04-12 18:11:47 +0200

    Update coucou.py

--- code/resume_generation/coucou.py
+++ code/resume_generation/coucou.py
@@ -82,4 +82,33 @@ print(outputs[0].outputs[0].text)
 
 
 
+#### à tester (source: https://docs.vllm.ai/en/latest/features/quantization/auto_awq.html)
+
+
+from vllm import LLM, SamplingParams
+
+# Sample prompts.
+prompts = [
+    "Hello, my name is",
+    "The president of the United States is",
+    "The capital of France is",
+    "The future of AI is",
+]
+# Create a sampling params object.
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+
+# Create an LLM.
+llm = LLM(model="TheBloke/Llama-2-7b-Chat-AWQ", quantization="AWQ")
+# Generate texts from the prompts. The output is a list of RequestOutput objects
+# that contain the prompt, generated text, and other information.
+outputs = llm.generate(prompts, sampling_params)
+# Print the outputs.
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+
+
+
 
