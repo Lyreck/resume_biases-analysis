3e6d2546a9be826316b0110770ecc11d8b9c286d
Léo le Douarec <leo.alex@le.douar.ec>
2025-05-24 22:15:57 +0200

    add init files to start building the code as a moudle

---
+++ code/resume_generation/__init__.py
@@ -0,0 +1,8 @@
+import logging
+# Get the logger
+logger = logging.getLogger(__name__)
+# Set the logging level
+logger.setLevel(logging.INFO)
+
+print("Importing Resume Generation Functions...")
+
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 
-
-
-
+from code.resume_generation.snippet_generation import generate_descriptions
+# en gros dans le init je mets direct les packages que je veux importer :)))))))))))
 
 if __name__ == "__main__":
     # To run, you need to specify the following parameters:
-    resume_characteristics = sys.argv[1]
+    resume_characteristics = sys.argv[1] # filepath to a .csv containing everything we need to generate resumes.
+    # This .csv should have the following columns: name,surname,british,volunteering,gender,tech_comp,med_comp,educ_comp,field_study
+    # Convention: 1 is british, 0 is not. 1 is female, 0 is male.
