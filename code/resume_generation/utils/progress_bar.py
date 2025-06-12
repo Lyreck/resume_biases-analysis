559d450562800495cc1ba3d5c23a94b1a97b5c32
Leo <63649449+Lyreck@users.noreply.github.com>
2025-06-12 23:24:33 +0200

    Delete code directory

--- code/resume_generation/utils/progress_bar.py
+++
@@ -1,6 +0,0 @@
-def progress(percent=0, width=30):
-    left = width * percent // 100
-    right = width - left
-    print('\r[', '#' * left, ' ' * right, ']',
-          f' {percent:.0f}%',
-          sep='', end='', flush=True)
